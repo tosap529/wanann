cf9f3eae8dc5acae6bc740b90889f327d76f99b1
ghesipig <ghesipig@gmail.com>
2024-02-07 11:37:24 +0800

    set router

--- src/router/index.js
+++ src/router/index.js
@@ -17,10 +17,131 @@ const router = createRouter({
       component: () => import('../views/about.vue')
     },
     {
+      path: '/act',
+      name: 'act',
+      component: () => import('../views/act.vue')
+    },
+    {
+      path: '/article1',
+      name: 'article1',
+      component: () => import('../views/article1.vue')
+    },
+    {
       path: '/articles',
       name: 'articles',
       component: () => import('../views/articles.vue')
-    }
+    },
+    {
+      path: '/bMember',
+      name: 'bMember',
+      component: () => import('../views/bMember.vue')
+    },
+    {
+      path: '/case',
+      name: 'case',
+      component: () => import('../views/case.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/contact.vue')
+    },
+    {
+      path: '/forget',
+      name: 'forget',
+      component: () => import('../views/forget.vue')
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: () => import('../views/game.vue')
+    },
+    {
+      path: '/gameQ',
+      name: 'gameQ',
+      component: () => import('../views/gameQ.vue')
+    },
+    {
+      path: '/gameR',
+      name: 'gameR',
+      component: () => import('../views/gameR.vue')
+    },
+    {
+      path: '/mall',
+      name: 'mall',
+      component: () => import('../views/mall.vue')
+    },
+    {
+      path: '/member',
+      name: 'member',
+      component: () => import('../views/member.vue')
+    },
+    {
+      path: '/mItem',
+      name: 'mItem',
+      component: () => import('../views/mItem.vue')
+    },
+    {
+      path: '/mPay1',
+      name: 'mPay1',
+      component: () => import('../views/mPay1.vue')
+    },
+    {
+      path: '/mPay2',
+      name: 'mPay2',
+      component: () => import('../views/mPay2.vue')
+    },
+    {
+      path: '/mPay3',
+      name: 'mPay3',
+      component: () => import('../views/mPay3.vue')
+    },
+    {
+      path: '/planA',
+      name: 'planA',
+      component: () => import('../views/planA.vue')
+    },
+    {
+      path: '/reserve1',
+      name: 'reserve1',
+      component: () => import('../views/reserve1.vue')
+    },
+    {
+      path: '/reserve2',
+      name: 'reserve2',
+      component: () => import('../views/reserve2.vue')
+    },
+    {
+      path: '/reserve3',
+      name: 'reserve3',
+      component: () => import('../views/reserve3.vue')
+    },
+    {
+      path: '/service',
+      name: 'service',
+      component: () => import('../views/service.vue')
+    },
+    {
+      path: '/sPay1',
+      name: 'sPay1',
+      component: () => import('../views/sPay1.vue')
+    },
+    {
+      path: '/sPay2',
+      name: 'sPay2',
+      component: () => import('../views/sPay2.vue')
+    },
+    {
+      path: '/sPay3',
+      name: 'sPay3',
+      component: () => import('../views/sPay3.vue')
+    },
+    {
+      path: '/welcome',
+      name: 'welcome',
+      component: () => import('../views/welcome.vue')
+    },
+    
 
   ]
 })
---
+++ src/views/mItem.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="mitem_wrapper wrapper">
+        <section class="mitem_sec">
+
+            <div class="mitem_main">
+
+                <div class="mitem_photo">
+                    <ul class="mitem_photo_small">
+                        <li><img src="@/img/mall/mall_product1_1.jpg"></li>
+                        <li><img src="@/img/mall/mall_product1_2.jpg"></li>
+                        <li><img src="@/img/mall/mall_product1_3.jpg"></li>
+                    </ul>
+                    <div class="mitem_photo_big">
+                        <img src="@/img/mall/mall_product1_1.jpg">
+                    </div>
+                </div>
+
+                <div class="mitem_product">
+                    <h1>浣安手工香皂</h1>
+                    <h2>NTD200</h2>
+                    <p><i class="fa-solid fa-thumbtack mitem_pin"></i>天然香潔，親手愛呵護。</p>
+                    <p><i class="fa-solid fa-thumbtack mitem_pin"></i>淨化心靈，手工皂香。</p>
+                    <h3>款式</h3>
+
+                    <div class="mitem_design">
+                        <h4>經典款</h4>
+                    </div>
+
+                    <h3>數量</h3>
+
+                    <div class="cal">
+                        <i class="fa-solid fa-minus cal_btn"></i>
+                        <div class="cal_count">1</div>
+                        <i class="fa-solid fa-plus cal_btn"></i>
+                    </div>
+
+                    <div class="mitem_btn">
+                        <button class="btn mitem_btn"><i class="fa-solid fa-cart-shopping mitem_add_to_cart"></i>加入購物車</button>
+                        <button class="btn mitem_btn">直接購買</button>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="mitem_tab tab">
+                <ul>
+                    <li><a href="#">商品敘述</a></li>
+                    <li><a href="#">款式說明</a></li>
+                    <li><a href="#">運送方式</a></li>
+                </ul>
+            </div>
+            <ol class="mitem_text">
+                <li><p>純天然成分，呵護您的肌膚，賦予自然清新香氣。</p></li>
+                <li><p>手工製作，每塊香皂獨一無二，富含滋潤護膚精華。</p></li>
+            </ol>
+
+        </section>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '@/sass/main.scss';
+</style>
