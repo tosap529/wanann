33eb0a0881ccce1fcd26350998b5b2da5164efc8
dxwang0620 <dxwang0620@gmail.com>
2024-02-18 12:55:59 +0800

    add some vue

--- src/views/act.vue
+++ src/views/act.vue
@@ -2,7 +2,8 @@
     <div>
         <div class="wrapper">
 
-<section>
+<section class="act_tab">
+
     <div class="tab">
         <ul>
             <li class="tab_on"><a href="#">優惠活動</a></li>
@@ -21,7 +22,7 @@
             <h4>新的一年到了，是時候給家居進行一番除舊佈新的大改變了！我們為您提供了獨家的優惠活動，只需在購物時輸入優惠碼，即可立即享受100元折扣。這是一個難得的機會，讓您以更實惠的價格，更新您家中的各種家居用品以及獲得我們的折扣服務。
             </h4>
             <div class="coupon_container">
-                <input type="text" value="G1d96PT32f147" class="coupon_input" readonly="readonly"></input>
+                <input type="text" value="G1d96PT32f147" class="coupon_input" readonly="readonly">
                 <button class="copy_button" id="copy_btn">複製</button>
             </div>
             <h3>使用期限至 2024-02-07 </h3>
@@ -34,8 +35,7 @@
             <h2>歡慶5週年，折扣大放送 </h2>
             <h4>親愛的顧客，感謝您在過去五年來的一路相伴與支持，我們的五週年慶典終於來臨了！為了回饋您的支持與信任，我們決定推出一場盛大的折扣大放送！有消費紀錄的會員皆可獲得200元大禮包~</h4>
             <div class="coupon_container">
-                <input type="text" value="Z896tg321q21" class="coupon_input" readonly="readonly"></input>
-                <button class="copy_button">複製</button>
+                <input type="text" value="Z896tg321q21" class="coupon_input" readonly="readonly">                <button class="copy_button">複製</button>
             </div>
             <h3>使用期限至 2024-01-20 </h3>
         </div>
@@ -47,7 +47,7 @@
             <h2>聖誕節快樂！獨家折扣驚喜等您！</h2>
             <h4>沒事就來瀏覽我們的商城，不時你會發現許多驚喜的折扣跟商品優惠，讓您的聖誕購物之旅能更加有趣且又經濟實惠。這個聖誕節，讓我們一同分享愛與喜悅，盡情享受購物的樂趣吧！祝您聖誕快樂！</h4>
             <div class="coupon_container">
-                <input type="text" value="G81PT123d2824" class="coupon_input" readonly="readonly"></input>
+                <input type="text" value="G81PT123d2824" class="coupon_input" readonly="readonly">
                 <button class="copy_button">複製</button>
             </div>
             <h3>使用期限至 2023-12-31 </h3>
@@ -61,7 +61,7 @@
             <h4>我們為您挑選了一系列精心設計的禮物，涵蓋了從cp值極高的服務內容到實用家居用品的多個品類。所有商品將在活動期間享有特別優惠價格，折扣88元，讓您可以用心選擇一份特別的禮物，為父親帶來驚喜。
             </h4>
             <div class="coupon_container">
-                <input type="text" value="A835TW321193" class="coupon_input" readonly="readonly"></input>
+                <input type="text" value="A835TW321193" class="coupon_input" readonly="readonly">
                 <button class="copy_button">複製</button>
             </div>
             <h3>使用期限至 2023-08-11 </h3>
@@ -74,7 +74,21 @@
 
 <script>
     export default {
+        data(){
+
+        },
+        mounted(){
+            let coupon_input_el = document.getElementsByClassName("coupon_input");
+    let copy_btns = document.getElementsByClassName("copy_button");
+
+    for(let i = 0; i < copy_btns.length; i++){
         
+        copy_btns[i].addEventListener("click", function(){
+          coupon_input_el[i].select();
+          document.execCommand('copy');
+        });
+    }
+        }
     }
 </script>
 
