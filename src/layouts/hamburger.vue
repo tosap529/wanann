ca213d84eba65ca7167197629f6f7102e4e046e2
tosap529 <tosap529@gmail.com>
2024-02-19 21:00:01 +0800

    hamburger 合併

--- src/layouts/hamburger.vue
+++ src/layouts/hamburger.vue
@@ -12,7 +12,7 @@
                 <span class="bar"></span>
             </div>
             <div class="nav_left">
-                <a href="#"><img src="../img/logo_square.svg" alt="logo"></a>
+                <a href="#"><img src="@/img/logo_square.svg" alt="logo"></a>
             </div>
 
             <div class="nav_right">
@@ -51,6 +51,114 @@
 
 <style lang="scss">
 @import '@/sass/main.scss';
-@import '../sass/layout/hamburger';
+
+body {
+    @include m() {
+        background-color: $dark-milktea;
+    }
+}
+
+@include m() {
+    a {
+        color: $black;
+    }
+
+    nav {
+        position: relative;
+
+        .hamburger {
+            padding-top: 8px;
+            position: absolute;
+            top: -92px;
+            right: 25px;
+
+            .bar {
+                display: block;
+                background-color: $black;
+                width: 25px;
+                height: 2px;
+                margin: 5px auto;
+                transition: all 0.3s ease-in-out;
+
+            }
+        }
+
+        .nav_left {
+            position: absolute;
+            top: -67px;
+            left: 38%;
+
+            img {
+                width: 100px;
+            }
+        }
+
+        .nav_right {
+            margin: 104px auto;
+            background-color: $light-milktea;
+            width: 380px;
+            height: 670px;
+            border-radius: 30px;
+            position: relative;
+            z-index: -1;
+
+            ul {
+                position: absolute;
+                top: 52px;
+                left: 20%;
+
+                li {
+                    margin-bottom: 24px;
+                    padding-left: 15px;
+                }
+
+                .h_li {
+                    background-color: $white;
+                    padding: 15px 20px 1px 15px;
+                    border-radius: 10px;
+                }
+
+                ol {
+                    margin-left: 100px;
+                    padding-left: 8px;
+                    margin-top: 16px;
+                    border-left: 4px solid $brown;
+                    border-radius: 4px;
+                }
+
+
+            }
+
+            .h_btns {
+                display: flex;
+                width: 320px;
+                border-top: 1px dashed $brown;
+                position: absolute;
+                bottom: 20px;
+                left: 8%;
+                padding-top: 16px;
+
+                .btn {
+                    padding: 8px 12px;
+                    margin-left: 45px;
+                    margin-right: 24px;
+
+                    img {
+                        display: inline;
+                        padding-right: 8px;
+                        vertical-align: sub;
+                    }
+                }
+
+                a {
+                    padding: 0 10px;
+                    padding-top: 6px;
+                }
+
+            }
+        }
+    }
+
+}
 
 </style>
