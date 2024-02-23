89a02cd7a357c589ea06c68d661be675ecf69588
ghesipig <ghesipig@gmail.com>
2024-02-23 16:19:55 +0800

    modify router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  
+  // 原先的沒有刪除
+  //history: createWebHistory(import.meta.env.BASE_URL),
+
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
