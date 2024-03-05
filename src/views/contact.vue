cae2fd0e921cfff96dced1d591a4d0e751a3c68a
dxwang0620 <dxwang0620@gmail.com>
2024-03-05 14:50:14 +0800

    add contact

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -22,7 +22,9 @@ const banner_url = BannerUrl;
     // };
 
     const submitForm = () => {
-    const url = 'http://localhost/WANANN/public/php/contact.php';
+    const url = 'http://localhost/thd104/public/php/contact_insert.php';
+
+    
     
     fetch(url, {
         method: 'POST',
