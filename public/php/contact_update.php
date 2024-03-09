9d4d3d04185f9699eb57454087f2730598596286
dxwang0620 <dxwang0620@gmail.com>
2024-03-09 14:14:24 +0800

    add cms product_order.php

---
+++ public/php/contact_update.php
@@ -0,0 +1,20 @@
+<?php
+
+include("connect_test.php");
+$reqBody = json_decode(file_get_contents("php://input"), true);
+
+$pdo = getPDO();
+$statement = $pdo->prepare("update CONTACT_TABLE set STATUS = :status, NAME = :name, PHONE = :phone, EMAIL = :email, CONTENT = :content, CREATE_TIME = :createTime where ID = :id");
+
+$statement->bindValue(":status", $reqBody["status"]);
+$statement->bindValue(":id", $reqBody["id"]);
+$statement->bindValue(":phone", $reqBody["phone"]);
+$statement->bindValue(":email", $reqBody["email"]);
+$statement->bindValue(":content", $reqBody["content"]);
+$statement->bindValue(":createTime", $reqBody["createTime"]);
+$statement->bindValue(":name", $reqBody["name"]);
+$statement->execute();
+
+json_encode($statement);
+
+?>
