a38d552568e249544e9b4cc1e16290b287132e9b
joannelu0712 <joannelu0712@gmail.com>
2024-03-10 13:12:46 +0800

    add email validation, complete forget page func.

---
+++ public/php/forget_select.php
@@ -0,0 +1,22 @@
+<?php
+include("connect_test.php");
+// include("connect.php");
+
+$sql = "SELECT ID FROM member where EMAIL= :email";
+$pdo = getPDO();    
+$statement = $pdo->prepare($sql);
+$statement->bindValue(":email",  $reqBody["email"]);
+
+$statement->execute();
+
+$data = $statement->fetch();
+
+// echo $data;
+if($data!=null){
+    echo $data['ID'];
+
+}else{
+    echo 'failed';
+}
+
+?>
