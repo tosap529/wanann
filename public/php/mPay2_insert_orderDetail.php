885bde5eebde857df43433b15d03c28502baf2ca
tosap529 <tosap529@gmail.com>
2024-03-11 22:55:29 +0800

    修復結帳PHP

--- public/php/mPay2_insert_orderDetail.php
+++ public/php/mPay2_insert_orderDetail.php
@@ -1,25 +1,25 @@
 <?php
-include("connect.php");
+// include("connect.php");
 
-$pdo = getPDO();
+// $pdo = getPDO();
 
-$reqBody = json_decode(file_get_contents("php://input"), true);
-$orderItems = $reqBody['orderItemDetail'];
+// $reqBody = json_decode(file_get_contents("php://input"), true);
+// $orderItems = $reqBody['orderItemDetail'];
 
-foreach ($orderItems as $item) {
-    $statement = $pdo->prepare("
-        INSERT INTO `Wanann_database`.`PRODUCT_ORDER_DETAIL` 
-        (`QUANTITY`, `PRODUCT_ID`, `PRODUCT_ORDER_ID`) 
-        VALUES (:quantity, :productId, :productOrderId);
-    ");
+// foreach ($orderItems as $item) {
+//     $statement = $pdo->prepare("
+//         INSERT INTO `Wanann_database`.`PRODUCT_ORDER_DETAIL` 
+//         (`QUANTITY`, `PRODUCT_ID`, `PRODUCT_ORDER_ID`) 
+//         VALUES (:quantity, :productId, :productOrderId);
+//     ");
 
-    $statement->bindValue(":quantity", $item["QUANTITY"]);
-    $statement->bindValue(":productId", $item["PRODUCT_ID"]);
-    $statement->bindValue(":productOrderId", $item["PRODUCT_ORDER_ID"]);
-    $statement->execute();
-}
+//     $statement->bindValue(":quantity", $item["QUANTITY"]);
+//     $statement->bindValue(":productId", $item["PRODUCT_ID"]);
+//     $statement->bindValue(":productOrderId", $item["PRODUCT_ORDER_ID"]);
+//     $statement->execute();
+// }
 
-echo "訂單商品提交成功";
+// echo "訂單商品提交成功";
 
 
 ?>
